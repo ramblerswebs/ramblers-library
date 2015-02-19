8e6f17c57846da4f66ededf97b2e352163486541
Chris Vaughan <chris@cevsystems.co.uk>
2015-02-19 09:18:27 +0000

    version 0.9.3

--- js/ramblersleaflet.js
+++ js/ramblersleaflet.js
@@ -2,7 +2,7 @@ function addWalk($list, $class, $date, $title, $dist, $gr, $lat, $long, $url, $i
 {
     var marker = L.marker([$lat, $long], {icon: $icon});
     var $walk = "<div class='" + $class + "'><b><a href='" + $url + "' target='_blank'>" + $date + "<br/>" + $title + "<br/>" + $dist + "</a></b></div>";
-    // var $map = "<a href='http://streetmap.co.uk/loc/" + $lat + "," + $long + "' target='_blank'>[OS Map]</a>";
+    var $walk = "<div class='" + $class + "'><b><a href=\"javascript:walkdetails('" + $url + "')\" >" + $date + "<br/>" + $title + "<br/>" + $dist + "</a></b></div>";
     var $map = "<a href=\"javascript:streetmap('" + $gr + "')\" >[OS Map]</a>";
     var $directions = "<a href=\"javascript:directions(" + $lat + "," + $long + ")\" >[Directions]</a>";
     //  var $directions = "<a href='https://maps.google.com?saddr=Current+Location&daddr=" + $lat + "," + $long + "' target='_blank'>[Directions]</a>";
@@ -11,6 +11,11 @@ function addWalk($list, $class, $date, $title, $dist, $gr, $lat, $long, $url, $i
     marker.bindPopup($popup);
     $list.push(marker);
 
+}
+
+function walkdetails($url) {
+    page = $url;
+    window2 = open(page, "_blank", "scrollbars=yes,width=990,height=480,menubar=yes,resizable=yes,status=yes");
 }
 
 function photos($gr) {
