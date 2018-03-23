a25d2a216b8d0095dac2beb2ac2088f186c3bb8c
Chris Vaughan <chris@cevsystems.co.uk>
2018-03-23 14:23:00 +0000

    Test multiple GPX file option

--- leaflet/gpx/map.php
+++ leaflet/gpx/map.php
@@ -9,6 +9,7 @@ class RLeafletGpxMap extends RLeafletMap {
 
     public $linecolour = "#782327";
     public $imperial = false;
+    public $addDownloadLink = true;
 
     public function __construct() {
         parent::__construct();
@@ -29,7 +30,17 @@ class RLeafletGpxMap extends RLeafletMap {
         $text = "displayGPX(ramblersMap, '$gpx', '$this->linecolour', $imperial)";
         parent::addContent($text);
         if (file_exists($gpx)) {
-            parent::display();
+            $path_parts = pathinfo($gpx);
+            if (strtolower($path_parts['extension']) == "gpx") {
+                parent::display();
+                if ($this->addDownloadLink) {
+                    echo "<br/><p><a href=$gpx>Download the gpx file for this walk</a></p>";
+                }
+            } else {
+                $application = JFactory::getApplication();
+                $application->enqueueMessage(JText::_('GPX: Route file is not a gpx file: ' . $file), 'error');
+                echo "<p><b>Unable to display gpx file</b></p>";
+            }
         } else {
             $application = JFactory::getApplication();
             $application->enqueueMessage(JText::_('GPX: Route file not found: ' . $file), 'error');
---
+++ leaflet/gpx/mapget.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Description of RLeafletGpxMapget
+ *    Display Gpx file on map but use file name from GET
+ *
+ * @author Chris Vaughan
+ */
+class RLeafletGpxMapget extends RLeafletGpxMap {
+
+    Public $folder = "images";
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function displayPath($name = "") {
+        $opts = new RLeafletGpxMap();
+        $gpxname = $_GET['gpx'];
+        if ($this->folder != "") {
+            $gpx = $this->folder . "/" . $gpxname . ".gpx";
+        } else {
+            $gpx = $gpxname . ".gpx";
+        }
+        if ($this->checkFilename($gpx)) {
+            parent::displayPath($gpx);
+        }
+    }
+
+    private function checkFilename($name) {
+        if (!strpos($name, "://") === false) {
+            $application = JFactory::getApplication();
+            $application->enqueueMessage(JText::_('GPX: Route file name is invalid: ' . $name), 'error');
+            echo "<p><b>Unable to display gpx file</b></p>";
+            return false;
+        }
+        return true;
+    }
+
+}
