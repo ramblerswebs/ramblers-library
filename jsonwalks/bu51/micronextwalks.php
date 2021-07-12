edb92f63127219b1b272d2c65bbb1391754b827f
Chris Vaughan <chris@cevsystems.co.uk>
2021-07-12 11:09:53 +0100

    BU51 fixes

--- jsonwalks/bu51/micronextwalks.php
+++ jsonwalks/bu51/micronextwalks.php
@@ -10,16 +10,17 @@ defined("_JEXEC") or die("Restricted access");
 
 class RJsonwalksBU51Micronextwalks extends RJsonwalksStdSimplelist {
 
-    public $walkClass = "nextwalk";
+    public $walkClass = "bu51-nextwalk";
     public $feedClass = "walksfeed"; // not used?
-    private $listFormat = ["{dowShortddmm}","{startTime}",
-        "{,title}","{,startPC}","{,nationalGrade}", "{,distance}"];
+    private $listFormat = ["{dowShortddmm}", "{startTime}",
+        "{,title}", "{,startPC}", "{,nationalGrade}", "{,distance}"];
     private $nowalks = 5;
 
     public function __construct() {
         parent::__construct();
         parent::customFormat($this->listFormat);
         parent::setWalksClass($this->walkClass);
+        RLoad::addStyleSheet("libraries/ramblers/jsonwalks/bu51/bu51style.css", "text/css");
     }
 
     public function noWalks($no) {
