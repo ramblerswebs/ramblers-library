d9bd68410e3cf072350d98eefc0ccdb61c3d7ba9
Chris Vaughan <chris@cevsystems.co.uk>
2022-01-31 17:37:17 +0000

    Early IOS bug fixes

--- load/load.php
+++ load/load.php
@@ -2,7 +2,7 @@
 
 class RLoad {
 
-    private static $revisionversion = "4.2.5";
+    private static $revisionversion = "4.2.6";
 
     public static function addScript($path, $type = "text/javascript") {
         $document = JFactory::getDocument();
