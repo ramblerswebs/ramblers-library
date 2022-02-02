931e9e303a4f297ffb3aca59a5fc5fe3ec959210
Chris Vaughan <chris@cevsystems.co.uk>
2022-02-02 14:06:22 +0000

    fixes

--- load/load.php
+++ load/load.php
@@ -2,7 +2,7 @@
 
 class RLoad {
 
-    private static $revisionversion = "4.2.6";
+    private static $revisionversion = "4.2.7";
 
     public static function addScript($path, $type = "text/javascript") {
         $document = JFactory::getDocument();
