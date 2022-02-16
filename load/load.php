002dcd8c4ee5b6426ac97158004111f299b53669
Chris Vaughan <chris@cevsystems.co.uk>
2022-02-16 10:14:47 +0000

    IOS bug fix for calendar tab

--- load/load.php
+++ load/load.php
@@ -2,7 +2,7 @@
 
 class RLoad {
 
-    private static $revisionversion = "4.2.9";
+    private static $revisionversion = "4.2.11";
 
     public static function addScript($path, $type = "text/javascript") {
         $document = JFactory::getDocument();
